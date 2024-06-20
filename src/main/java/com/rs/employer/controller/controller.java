fd2ed750435375ecb02dd47d578b36b61da932e3
ilovepakpak@gmail.com <Chessy>
2024-06-20 18:11:03 +0700

    CrossOrigin Change

--- src/main/java/com/rs/employer/controller/controller.java
+++ src/main/java/com/rs/employer/controller/controller.java
@@ -3,6 +3,7 @@ package com.rs.employer.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ import com.rs.employer.service.CustomerImplement;
 
 @RequestMapping(path = "/v1")
 @RestController
+@CrossOrigin
 public class controller {
     // public final CustomerRepository customerRepository;
 
