d0952dca0f0f880136ea948bb6cf8e3ef882d7fe
ilovepakpak@gmail.com <Chessy>
2024-06-24 15:31:45 +0700

    Properties auto update --rebase React --init

--- src/main/resources/templates/react_customer/src/App.js
+++ src/main/resources/templates/react_customer/src/App.js
@@ -1,27 +1,83 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import './App.css';
 const opts = { 
   method: 'GET',
   headers: { 'Content-Type': 'application/json' },
   body: JSON.stringify({ title: 'React POST Request Example' })
 }
-function Render({opts}) {
+
+export function Home () {
+  return (
+   <div>
+   <nav>
+      <Link to="/">Home</Link>
+      <Link to="/parse">Data</Link>
+      <Link to="/contact">Contact</Link>
+      <Link to="/about">About</Link>
+    </nav>
+    </div>
+  )
+}
+export function Data () {
+  <div>
+  <nav>
+     <Link to="/">Home</Link>
+     <Link to="/parse">Data</Link>
+     <Link to="/contact">Contact</Link>
+     <Link to="/about">About</Link>
+   </nav>
+   </div>
+}
+export function Contact () {
+  return (
+    <div>
+    <nav>
+       <Link to="/">Home</Link>
+       <Link to="/parse">Data</Link>
+       <Link to="/contact">Contact</Link>
+       <Link to="/about">About</Link>
+     </nav>
+     </div>
+  )
+}
+export function About() {
+  return (
+    <div>
+    <nav>
+       <Link to="/">Home</Link>
+       <Link to="/parse">Data</Link>
+       <Link to="/contact">Contact</Link>
+       <Link to="/about">About</Link>
+     </nav>
+     </div>
+  )
+}
+function Display(){
+  return (<div>
+      <Home />
+      <Data />
+      <Contact /> 
+      <About />
+  </div>)
+}
+export function Render({opts}) {
   const [api , getAPI] = useState(null);
   useEffect(() => {
-    fetch(`https://employer-ksml.onrender.com/api/customer/allcustomer`,opts.opts)
+    fetch(`https://employer-ksml.onrender.com/api/customer/allcustomer`,opts)
     .then((respone) => respone.json())
     .then(data => getAPI(data))
     .catch(err => console.log(err))
   }, []);
-  if(!api) return(<p>Data is still fetching </p>) 
-  return (<pre>
-    {JSON.stringify(api , null,1)}
+    if(!api) return(<p>Fetching data...</p>) 
+      return (<pre>
+    {JSON.stringify(api , null,2)}
     </pre>)
 }
 function App() {
-  return (
-    <div className="App"> 
-      <Render opts={opts}  />
+    return (
+    <div> 
+      <Home />
     </div>
   );
 }
