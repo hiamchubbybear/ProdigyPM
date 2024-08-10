b6c9143c37f7a8213a22828af09936efa43e4b08
Chessy <ilovepakpak@gmail.com>
2024-08-10 16:06:34 +0700

    Sat 10th Aug { Mapper (fix)}

--- src/main/java/com/rs/employer/mapper/CustomerMapper.java
+++ src/main/java/com/rs/employer/mapper/CustomerMapper.java
@@ -1,6 +1,7 @@
 package com.rs.employer.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.rs.employer.dto.Request.CustomerRequest;
 import com.rs.employer.dto.Respone.CustomerRespone;
@@ -8,8 +9,10 @@ import com.rs.employer.model.Customer;
 
 @Mapper(componentModel = "spring")
 public interface CustomerMapper {
+    @Mapping(target = "role", ignore = true)
     CustomerRespone toCustomerRespone(CustomerRequest request);
 
+    @Mapping(target = "role", ignore = true)
     Customer toCustomer(CustomerRequest request);
 
     CustomerRespone toCustomerRespone(Customer request);
