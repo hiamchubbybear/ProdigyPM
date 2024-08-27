423fd79db798bc47e544cbc0d6fa9527474e945f
Chessy <ilovepakpak@gmail.com>
2024-08-27 18:39:31 +0700

    #110 27 Aug {Mapper(update) }

--- src/main/java/com/rs/employer/mapper/ProductMapper.java
+++ src/main/java/com/rs/employer/mapper/ProductMapper.java
@@ -10,7 +10,7 @@ import com.rs.employer.model.Product;
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
     ProductRespone toProductRespone(ProductRequest request);
-    @Mapping(ignore = true , target = "carts")
+    @Mapping(ignore = true , target = "cart")
     Product toProduct(ProductRequest request);
 
 }
