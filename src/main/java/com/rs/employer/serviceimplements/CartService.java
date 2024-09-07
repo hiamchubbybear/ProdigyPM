ae495492556b441cc40cfd84b5518dca4d665264
Chessy <ilovepakpak@gmail.com>
2024-09-07 15:32:46 +0700

    #122 Sat 7th Sep {Cart (Service , Controller , Model)}

--- src/main/java/com/rs/employer/serviceimplements/CartService.java
+++ src/main/java/com/rs/employer/serviceimplements/CartService.java
@@ -8,65 +8,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rs.employer.dao.CartRepository;
-import com.rs.employer.dao.ProductRepository;
 import com.rs.employer.globalexception.AppException;
 import com.rs.employer.globalexception.ErrorCode;
 import com.rs.employer.model.Cart;
-
-// Service implement for cart
-
 @Service
 public class CartService {
     @Autowired
     private CartRepository cartRepository;
-    Instant date;
-    @Autowired
-    private ProductRepository productRepository;
-
-
-    @Autowired
-
+    Instant date= Instant.now();
     public List<Cart> getAllCart() {
         return cartRepository.findAll();
     }
-
     public Cart addCart(Cart request) {
         if (!cartRepository.existsById(request.getId())) {
-            request.setCreate(date.now());
-            request.setUpdate(date.now());
-            // Set<Product> products = new HashSet<Product>(productRepository.findAllBy());
-            // request.setProducts(products);
+            request.setCreate(date);
+            request.setUpdate(date);
             return cartRepository.save(request);
         } else
             throw new AppException(ErrorCode.CART_NOT_FOUNT);
     }
-
-    // @PreAuthorize("hasAuthority('SCOPE_DELETE_PRODUCT')or
-    // hasAuthority('SCOPE_PERMIT_ALL')")
-
     public Boolean deleteCart(Long ID) {
-        if (!cartRepository.existsById(ID))
-            throw new AppException(ErrorCode.CART_NOT_FOUNT);
-        else {
-            cartRepository.deleteById(ID);
-            return true;
-        }
+        cartRepository.findById(ID).ifPresentOrElse(cartRepository::delete, () -> new AppException(ErrorCode.CART_NOT_FOUNT));
+        return true;
     }
-
     public Cart updateCart(Long ID, Cart request) {
         if (!cartRepository.existsById(ID)) {
             throw new AppException(ErrorCode.CART_NOT_FOUNT);
         } else {
             return cartRepository.save(request);
         }
-
     }
-
     public Optional<Cart> getCart(Long ID) {
         if (cartRepository.existsById(ID))
             return cartRepository.findById(ID);
         else
             throw new AppException(ErrorCode.CART_NOT_FOUNT);
     }
-
 }
